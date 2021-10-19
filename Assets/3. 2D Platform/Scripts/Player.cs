90d11377f6826c022cde8c9c1f3226cca6e97ba2
Ренат <syltan.100renrgen@gmail.com>
2021-10-19 20:52:17 +0300

    add audio and fix bug

--- Assets/3. 2D Platform/Scripts/Player.cs
+++ Assets/3. 2D Platform/Scripts/Player.cs
@@ -2,29 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class Player : MonoBehaviour
 {
     [SerializeField] private int _health;
-    [SerializeField] private int _countMoney;
+    [SerializeField] private AudioClip _takeDamegeSound;
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent<EnemyMovement>(out EnemyMovement enemy))
         {
             _health--;
+            _audioSource.PlayOneShot(_takeDamegeSound);  
 
             if (_health <= 0)
             {
                 Debug.Log("Вы проиграли!");
             }
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.TryGetComponent<Money>(out Money money))
-        {
-            _countMoney++;
         }
     }
 }
---
+++ Assets/3. 2D Platform/Scripts/PlayerMoney.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class PlayerMoney : MonoBehaviour
+{
+    [SerializeField] private int _countMoney;
+    [SerializeField] private AudioClip _moneySound;
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Money>(out Money money))
+        {
+            _audioSource.PlayOneShot(_moneySound);
+            _countMoney++;
+        }
+    }
+}
