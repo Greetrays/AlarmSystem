3d801a9eb657874d436b7c6c82e435ea4d330c60
Ренат <syltan.100renrgen@gmail.com>
2021-12-17 21:45:18 +0300

    fix health bar

---
+++ Assets/3. 2D Platform/Scripts/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private PlayerHealth _player;
+
+    private float _speed;
+    private Slider _healthBar;
+
+    private void Start()
+    {
+        _speed = 3f;
+        _healthBar = GetComponent<Slider>();
+        _healthBar.maxValue = _player.Health;
+        _healthBar.value = _player.Health;
+    }
+
+    private void Update()
+    {
+        if (_healthBar.value != _player.Health)
+        {
+            _healthBar.value = Mathf.MoveTowards(_healthBar.value, _player.Health, _speed * Time.deltaTime);
+        }
+    }
+}
--- Assets/3. 2D Platform/Scripts/PlayerHealth.cs
+++ Assets/3. 2D Platform/Scripts/PlayerHealth.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 
@@ -11,25 +7,14 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private float _health;
     [SerializeField] private AudioClip _takeDamegeSound;
     [SerializeField] private AudioClip _usedHealth;
-    [SerializeField] private Slider _healthBar;
-    [SerializeField] private float _speed;
 
     private AudioSource _audioSource;
 
     public float Health => _health;
-
-    private void Update()
-    {
-        if (_healthBar.value != _health)
-        {
-            _healthBar.value = Mathf.MoveTowards(_healthBar.value, _health, Time.deltaTime * _speed);
-        }
-    }
-
+    
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
-        _healthBar.maxValue = _healthBar.value = _health;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
